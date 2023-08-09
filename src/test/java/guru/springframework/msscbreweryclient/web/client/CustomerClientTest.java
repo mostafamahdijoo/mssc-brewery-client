f7646aee7ad1fd3294035006adcaa7741453cc71
M_Mahdijoo <m_mahdijoo@isc.co.ir>
2023-08-09 17:19:32 +0330

    CustomerClient crud and test methods added.

---
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/CustomerClientTest.java
@@ -0,0 +1,50 @@
+package guru.springframework.msscbreweryclient.web.client;
+
+import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import guru.springframework.msscbreweryclient.web.model.CustomerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URI;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class CustomerClientTest {
+
+    @Autowired
+    CustomerClient client;
+
+    @Test
+    void getCustomerById() {
+        CustomerDto customerDto = client.getCustomerById(UUID.randomUUID());
+
+        assertNotNull(customerDto);
+    }
+
+    @Test
+    void saveNewCustomer() {
+        //given
+        CustomerDto customerDto = CustomerDto.builder().name("New Customer").build();
+        URI uri = client.saveNewCustomer(customerDto);
+
+        assertNotNull(uri);
+
+        System.out.println(uri.toString());
+    }
+
+    @Test
+    void updateCustomer() {
+        //given
+        CustomerDto customerDto = CustomerDto.builder().name("New Customer").build();
+
+        client.updateCustomer(UUID.randomUUID(), customerDto);
+    }
+
+    @Test
+    void deleteCustomer() {
+        client.deleteCustomer(UUID.randomUUID());
+    }
+}
